a6345e44900dbbcf851983af0b07a593ef9a88da
André Formento <andreformento.sc@gmail.com>
2017-06-03 23:46:31 -0300

    Implement comparable on Point

--- src/main/java/com/formento/algorithmexercises/pointinrectangle/Point.java
+++ src/main/java/com/formento/algorithmexercises/pointinrectangle/Point.java
@@ -1,14 +1,19 @@
 package com.formento.algorithmexercises.pointinrectangle;
 
+import java.util.Comparator;
 import java.util.Objects;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 
 @Getter
 @RequiredArgsConstructor
-public class Point {
+public class Point implements Comparable<Point> {
 
+    @NonNull
     private final Integer x;
+    @NonNull
     private final Integer y;
 
     @Override
@@ -27,6 +32,15 @@ public class Point {
     @Override
     public final int hashCode() {
         return Objects.hash(x, y);
+    }
+
+    @Override
+    public int compareTo(Point other) {
+        final int compare = Integer.compare(x, other.x);
+        if (compare == 0) {
+            return Integer.compare(y, other.y);
+        }
+        return compare;
     }
 
 }
--- src/test/java/com/formento/algorithmexercises/pointinrectangle/PointTest.java
+++ src/test/java/com/formento/algorithmexercises/pointinrectangle/PointTest.java
@@ -1,11 +1,18 @@
 package com.formento.algorithmexercises.pointinrectangle;
 
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.Matchers.allOf;
+import static org.hamcrest.Matchers.contains;
+import static org.hamcrest.Matchers.hasProperty;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertThat;
 
 import br.com.six2six.fixturefactory.Fixture;
 import br.com.six2six.fixturefactory.loader.FixtureFactoryLoader;
 import com.formento.algorithmexercises.pointinrectangle.template.PointTemplate;
+import com.google.common.collect.ImmutableSortedSet;
+import java.util.Collection;
 import nl.jqno.equalsverifier.EqualsVerifier;
 import org.junit.BeforeClass;
 import org.junit.Test;
@@ -31,6 +38,34 @@ public class PointTest {
     @Test
     public void equalsContract() {
         EqualsVerifier.forClass(Point.class).verify();
+    }
+
+    @Test
+    public void shouldBeValidComparatorByNatural() {
+        // given
+        final Point point_1_2 = Fixture.from(Point.class).gimme(PointTemplate.P_1_2);
+        final Point point_2_0 = Fixture.from(Point.class).gimme(PointTemplate.P_2_0);
+        final Point point_2_3 = Fixture.from(Point.class).gimme(PointTemplate.P_2_3);
+        final Point point_2_5 = Fixture.from(Point.class).gimme(PointTemplate.P_2_5);
+        final Point point_3_0 = Fixture.from(Point.class).gimme(PointTemplate.P_3_0);
+
+        // when
+        final Collection<Point> points = ImmutableSortedSet.<Point>naturalOrder().add(
+            point_2_3,
+            point_1_2,
+            point_2_5,
+            point_3_0,
+            point_2_0
+        ).build();
+
+        // then
+        assertThat(points, contains(
+            allOf(hasProperty("x", is(1)), hasProperty("y", is(2))),
+            allOf(hasProperty("x", is(2)), hasProperty("y", is(0))),
+            allOf(hasProperty("x", is(2)), hasProperty("y", is(3))),
+            allOf(hasProperty("x", is(2)), hasProperty("y", is(5))),
+            allOf(hasProperty("x", is(3)), hasProperty("y", is(0)))
+        ));
     }
 
 }
