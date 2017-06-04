c86b096f51021de17ab43d4dc0453b733edde02a
André Formento <andreformento.sc@gmail.com>
2017-06-04 00:09:36 -0300

    Implement comparable on Region

--- src/main/java/com/formento/algorithmexercises/pointinrectangle/Region.java
+++ src/main/java/com/formento/algorithmexercises/pointinrectangle/Region.java
@@ -1,15 +1,43 @@
 package com.formento.algorithmexercises.pointinrectangle;
 
-import java.util.List;
+import com.google.common.collect.ImmutableList;
+import com.google.common.collect.ImmutableSortedSet;
+import java.util.Collection;
 import java.util.Objects;
 import lombok.Getter;
-import lombok.RequiredArgsConstructor;
 
 @Getter
-@RequiredArgsConstructor
 public class Region {
 
-    private final List<Rectangle> rectangles;
+    private final Collection<Rectangle> rectangles;
+
+    private Region(final Collection<Rectangle> rectangles) {
+        this.rectangles = ImmutableSortedSet.<Rectangle>naturalOrder().addAll(rectangles).build();
+    }
+
+
+    public static class Builder {
+
+        private final ImmutableList.Builder<Rectangle> rectangles;
+
+        public Builder() {
+            this.rectangles = ImmutableList.builder();
+        }
+
+        public Builder addRectangle(final Rectangle... rectangle) {
+            this.rectangles.add(rectangle);
+            return this;
+        }
+
+        public Builder addRectangles(final Iterable<? extends Rectangle> rectangles) {
+            this.rectangles.addAll(rectangles);
+            return this;
+        }
+
+        public Region build() {
+            return new Region(rectangles.build());
+        }
+    }
 
     @Override
     public final boolean equals(Object o) {
--- src/test/java/com/formento/algorithmexercises/pointinrectangle/RegionTest.java
+++ src/test/java/com/formento/algorithmexercises/pointinrectangle/RegionTest.java
@@ -1,11 +1,19 @@
 package com.formento.algorithmexercises.pointinrectangle;
 
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.Matchers.allOf;
+import static org.hamcrest.Matchers.contains;
+import static org.hamcrest.Matchers.hasProperty;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertThat;
 
 import br.com.six2six.fixturefactory.Fixture;
 import br.com.six2six.fixturefactory.loader.FixtureFactoryLoader;
+import com.formento.algorithmexercises.pointinrectangle.template.PointTemplate;
+import com.formento.algorithmexercises.pointinrectangle.template.RectangleTemplate;
 import com.formento.algorithmexercises.pointinrectangle.template.RegionTemplate;
+import java.util.Collection;
 import nl.jqno.equalsverifier.EqualsVerifier;
 import org.junit.BeforeClass;
 import org.junit.Test;
@@ -31,6 +39,32 @@ public class RegionTest {
     @Test
     public void equalsContract() {
         EqualsVerifier.forClass(Region.class).verify();
+    }
+
+    @Test
+    public void shouldAddInXYPointOrder() {
+        // given
+        final Region region = new Region.Builder().addRectangle(
+            Fixture.from(Rectangle.class).gimme(RectangleTemplate.F),
+            Fixture.from(Rectangle.class).gimme(RectangleTemplate.B),
+            Fixture.from(Rectangle.class).gimme(RectangleTemplate.E),
+            Fixture.from(Rectangle.class).gimme(RectangleTemplate.D),
+            Fixture.from(Rectangle.class).gimme(RectangleTemplate.A),
+            Fixture.from(Rectangle.class).gimme(RectangleTemplate.C)
+        ).build();
+
+        // when
+        final Collection<Rectangle> result = region.getRectangles();
+
+        // then
+        assertThat(result, contains(
+            allOf(hasProperty("leftBottom", is(Fixture.from(Point.class).<Point>gimme(PointTemplate.P_0_0))), hasProperty("rightTop", is(Fixture.from(Point.class).<Point>gimme(PointTemplate.P_2_3)))),
+            allOf(hasProperty("leftBottom", is(Fixture.from(Point.class).<Point>gimme(PointTemplate.P_0_3))), hasProperty("rightTop", is(Fixture.from(Point.class).<Point>gimme(PointTemplate.P_2_5)))),
+            allOf(hasProperty("leftBottom", is(Fixture.from(Point.class).<Point>gimme(PointTemplate.P_1_2))), hasProperty("rightTop", is(Fixture.from(Point.class).<Point>gimme(PointTemplate.P_4_4)))),
+            allOf(hasProperty("leftBottom", is(Fixture.from(Point.class).<Point>gimme(PointTemplate.P_2_0))), hasProperty("rightTop", is(Fixture.from(Point.class).<Point>gimme(PointTemplate.P_4_3)))),
+            allOf(hasProperty("leftBottom", is(Fixture.from(Point.class).<Point>gimme(PointTemplate.P_2_3))), hasProperty("rightTop", is(Fixture.from(Point.class).<Point>gimme(PointTemplate.P_6_5)))),
+            allOf(hasProperty("leftBottom", is(Fixture.from(Point.class).<Point>gimme(PointTemplate.P_3_0))), hasProperty("rightTop", is(Fixture.from(Point.class).<Point>gimme(PointTemplate.P_6_3))))
+        ));
     }
 
 }
